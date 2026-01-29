7b402a026e18f332c1534851bd43e28b2d02c2a7
Kartik <kartik.kumar@unifyapps.com>
2026-01-29 21:58:49 +0530

    fixed date selection issue

--- components/CalenItem.tsx
+++ components/CalenItem.tsx
@@ -9,8 +9,13 @@ const CalenItem = ({dayNo,progress,selected,clicks}:CalenItemProps) => {
   const hue = (progress * 130) / 100;
 
   return (
-    <div className={'flex flex-col rounded-md border items-center px-5 py-1 border-[#d2c7f9] hover:bg-violet-100 ' + (selected ? "hello" : "bye")}
-      onClick={()=>clicks(dayNo)}
+    <div className={'flex flex-col rounded-md border items-center px-5 py-1 bg-violet-500 border-[#d2c7f9] hover:bg-violet-100 cursor-pointer'}
+      onClick={()=>{
+        clicks(dayNo)
+        console.log("clicked ",dayNo)
+      }
+      }
+      style={selected ?{backgroundColor:"#dcd5f8ff"} : {}}
     >
         <span className='text-xs text-start font-normal whitespace-pre-line'>Day</span>
         <span className='text-xl text-start font-medium -mt-xxs whitespace-pre-line'>{dayNo+1}</span>
